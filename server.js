3a10f48e20733b2fa206b400282c20487c76764c
Bill0106 <bill2010111@gmail.com>
2017-08-16 03:05:58 +0000

    connect database

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const routes = require('./app')
 const app = express()
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/codeShareService', { useMongoClient: true })
+mongoose.connection.on('error', console.error)
 
 app.use((req, res, next) => {
   const start = new Date().getTime()
