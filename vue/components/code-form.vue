053db197f6cbc1f714379e345a6534a1b546c998
bill <bill2010111@gmail.com>
2017-08-27 15:19:37 +0800

    code create

--- vue/components/code-form.vue
+++ vue/components/code-form.vue
@@ -1,6 +1,16 @@
 <template>
   <el-card class="box-card full-width">
-    <el-form :model="code">
+    <el-form :model="code" label-width="80px">
+      <el-form-item label="代码类型">
+        <el-select v-model="code.codeType">
+          <el-option v-for="(item, index) in codeTypes" :key="index" :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="应用场景">
+        <el-checkbox-group v-model="code.platforms">
+          <el-checkbox v-for="(item, index) in platforms" :label="item.value" :key="index">{{item.label}}</el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
       <el-form-item label="标题">
         <el-input v-model="code.title"></el-input>
       </el-form-item>
@@ -16,15 +26,47 @@
 </template>
 
 <script>
+import services from '@/services'
+
 export default {
   name: 'codeForm',
   data() {
     return {
+      codeTypes: [],
       code: {
         title: '',
-        code: ''
-      }
+        code: '',
+        codeType: '',
+        platforms: []
+      },
+      platforms: [
+        {
+          label: '服务器端',
+          value: 'SERVER'
+        },
+        {
+          label: '浏览器端',
+          value: 'BROWSER'
+        },
+        {
+          label: '客户端',
+          value: 'CLIENT'
+        },
+        {
+          label: '移动端',
+          value: 'MOBILE'
+        }
+      ]
     }
+  },
+  beforeMount() {
+    services.getCodeTypes()
+      .then(res => {
+        const { data } = res
+        if (data.status === 'success') {
+          this.codeTypes = data.data.list
+        }
+      })
   }
 }
 </script>
